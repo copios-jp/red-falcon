649bd78be43014d71bf7feeecad005e6c4e552e9
copios <copios.jp@gmail.com>
2018-11-19 16:18:34 +0900

    fix(keychain): more hacks on keychain

--- scripts/keychain.sh
+++ scripts/keychain.sh
@@ -12,14 +12,14 @@ security unlock-keychain -p $CERT_PASS $KEY_CHAIN
 # Set keychain locking timeout to 3600 seconds
 security set-keychain-settings -t 3600 -u $KEY_CHAIN
 
-- export APPLICATION_CERT=Applciation.p12
-- echo $CERT_OSX_DEVELOPER_ID_APPLICATION | base64 - --decode > $APPLICATION_CERT
+export APPLICATION_CERT=Applciation.p12
+echo $CERT_OSX_DEVELOPER_ID_APPLICATION | base64 - --decode > $APPLICATION_CERT
 
 # Add certificates to keychain and allow codesign to access them
 security import $APPLICATION_CERT -k $KEY_CHAIN -P $CERT_PASS -T /usr/bin/codesign
 
 echo "Add keychain to keychain-list"
-security list-keychains -s mac-build.keychain
+security list-keychains -s $KEY_CHAIN
 
 echo "Settting key partition list"
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $CERT_PASS $KEY_CHAIN
