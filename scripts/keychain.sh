abc1782fe0f479223b5b4249b0fa07d9906f8b94
copios <copios.jp@gmail.com>
2018-11-19 15:47:34 +0900

    fix(keychaing): include codesign in key partition list

--- scripts/keychain.sh
+++ scripts/keychain.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 KEY_CHAIN=mac-build.keychain
-security create-keychain -p travis $KEY_CHAIN
+security create-keychain -p $CERT_PASS $KEY_CHAIN
 
 # Make the keychain the default so identities are found
 security default-keychain -s $KEY_CHAIN
 
 # Unlock the keychain
-security unlock-keychain -p travis $KEY_CHAIN
+security unlock-keychain -p $CERT_PASS $KEY_CHAIN
 
 # Set keychain locking timeout to 3600 seconds
 security set-keychain-settings -t 3600 -u $KEY_CHAIN
@@ -22,5 +22,5 @@ echo "Add keychain to keychain-list"
 security list-keychains -s mac-build.keychain
 
 echo "Settting key partition list"
-security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $CERT_PASS $KEY_CHAIN
 
