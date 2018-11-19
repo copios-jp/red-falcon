70d78a19db8d3cfb51941e8999d56cdd7716d39d
copios <copios.jp@gmail.com>
2018-11-19 22:32:28 +0900

    fix(codesign): use Application cert, not installer

--- scripts/keychain.sh
+++ scripts/keychain.sh
@@ -15,15 +15,9 @@ security set-keychain-settings -t 3600 -u $KEY_CHAIN
 export APPLICATION_CERT=Applciation.p12
 echo $CERT_OSX_DEVELOPER_ID_APPLICATION | base64 - --decode > $APPLICATION_CERT
 
-
-export INSTALLER_CERT=Installer.p12
-echo $CERT_OSX_DEVELOPER_ID_INSTALLER | base64 - --decode > $INSTALLER_CERT
-
-
 # Add certificates to keychain and allow codesign to access them
 echo "Add certificate to keychain"
-# security import $APPLICATION_CERT -k $KEY_CHAIN -P $CERT_PASS -T /usr/bin/codesign
-security import $INSTALLER_CERT -k $KEY_CHAIN -P $CERT_PASS -T /usr/bin/codesign
+security import $APPLICATION_CERT -k $KEY_CHAIN -P $CERT_PASS -T /usr/bin/codesign
 
 echo "Add keychain to keychain-list"
 security list-keychains -d user -s $KEY_CHAIN
