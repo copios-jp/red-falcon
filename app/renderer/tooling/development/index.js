826d9105a9b9d48d80ded8ba6105a78d4274d735
copios <copios.jp@gmail.com>
2018-11-20 23:07:13 +0900

    chore(tooling): decomplexify apply

--- app/renderer/tooling/development/index.js
+++ app/renderer/tooling/development/index.js
@@ -1,58 +1,60 @@
 import { SensorsView } from '../../components/SensorsView'
 import EventEmitter from 'events'
 
-export default {
-  apply() {
+const addFakeChannel = function() {
+  const channels = this.state.channels
+  const id = Object.keys(channels).length++
+  const channel = {
+    channelId: id,
+    sensor: Object.assign(new EventEmitter(), { detach() {}, channel: id }),
+  }
+  channel.sensor.once('hbData', (data) => {
+    const state = { ...this.state }
+    const channel = state.channels[id]
+    channel.data = data
+    this.setState(state)
+  })
+  channels[id] = channel
+  this.setState(this.state)
 
-    SensorsView.prototype.addFakeChannel = function() {
+  return channel
+}
 
+const addFakeSensor = function() {
+  let beat = 80
+  const maxDelta = 5
+  let direction = 1
+  const channels = this.state.channels
+  const channel =
+    Object.values(channels).find((channel) => {
+      return channel.data === undefined
+    }) || this.addFakeChannel()
 
-      const channels = this.state.channels
-      const id = Object.keys(channels).length++
-        const channel = {
-        channelId: id,
-        sensor: Object.assign(new EventEmitter(), { detach() {}, channel: id }),
-      }
-      channel.sensor.once('hbData', (data) => {
-        const state = { ...this.state }
-        const channel = state.channels[id]
-        channel.data = data
-        this.setState(state)
-      })
-      channels[id] = channel
-      this.setState(this.state)
-
-      return channel
+  const interval = window.setInterval(() => {
+    if (beat > 160 || beat < 80) {
+      direction = direction * -1
     }
+    channel.sensor.emit('hbData', {
+      ComputedHeartRate: (beat += direction * Math.floor(Math.random() * Math.floor(maxDelta))),
+    })
+  }, 500)
 
-    SensorsView.prototype.addFakeSensor = function() {
-      let beat = 80
-      const maxDelta = 5
-      let direction = 1
-      const channels = this.state.channels
-      const channel = Object.values(channels).find((channel) => {
-        return channel.data === undefined
-      }) || this.addFakeChannel()
+  const detach = channel.sensor.detach
+  channel.sensor.detach = () => {
+    detach.call(channel.sensor)
+    window.clearInterval(interval)
+  }
 
-      const interval = window.setInterval(() => {
-        if (beat > 160 || beat < 80) {
-          direction = direction * -1
-        }
-        channel.sensor.emit('hbData', {
-          ComputedHeartRate: (beat += direction * Math.floor(Math.random() * Math.floor(maxDelta))),
-        })
-      }, 500)
-
-      const detach = channel.sensor.detach
-      channel.sensor.detach = () => {
-        detach.call(channel.sensor)
-        window.clearInterval(interval)
-      }
-      if (this.stick === undefined) {
-        this.stick = {
-          close() {},
-        }
-      }
+  if (this.stick === undefined) {
+    this.stick = {
+      close() {},
     }
   }
+}
+
+export default {
+  apply() {
+    SensorsView.prototype.addFakeChannel = addFakeChannel
+    SensorsView.prototype.addFakeSensor = addFakeSensor
+  },
 }
