5a3bc6850647fe6eeb19b73c5c154aa1be8b9d26
copios <copios.jp@gmail.com>
2018-11-22 17:51:54 +0900

    fix(format): format tooling

--- app/renderer/tooling/index.js
+++ app/renderer/tooling/index.js
@@ -2,4 +2,3 @@ import development from './development/'
 import production from './production/'
 import toolingFor from '../../services/tooling'
 export default toolingFor({ development, production })
-
