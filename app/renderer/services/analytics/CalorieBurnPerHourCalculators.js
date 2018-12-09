e9cfc964bf1ac49b024792c76133c288e4d42980
copios <copios.jp@gmail.com>
2018-12-10 00:04:04 +0900

    chore(duplication): extract calorie burn algo

--- app/renderer/services/analytics/CalorieBurnPerHourCalculators.js
+++ app/renderer/services/analytics/CalorieBurnPerHourCalculators.js
@@ -1,12 +1,12 @@
+const MALE_MODIFIER = -55.0969
 const MALE_RATE_COEF = 0.6309
 const MALE_WEIGHT_COEF = 0.1988
 const MALE_AGE_COEF = 0.2017
-const MALE_MODIFIER = -55.0969
 
+const FEMALE_MODIFIER = -20.4022
 const FEMALE_RATE_COEF = 0.4472
 const FEMALE_WEIGHT_COEF = 0.1263
 const FEMALE_AGE_COEF = 0.074
-const FEMALE_MODIFIER = -20.4022
 
 const REDUCER = 4.184
 const HOUR = 60
@@ -15,31 +15,19 @@ export const MALE = 'male'
 export const FEMALE = 'female'
 export const UNKNOWN = 'unknown'
 
+function calculator(MODIFIER, RATE_COEF, WEIGHT_COEF, AGE_COEF) {
+  return {
+    using(props) {
+      const { rate, weight, age } = props
+      return Math.round(
+        ((MODIFIER + RATE_COEF * rate - WEIGHT_COEF * weight + AGE_COEF * age) / REDUCER) * HOUR,
+      )
+    },
+  }
+}
 export const methods = {
-  [MALE]: {
-    using(props) {
-      const { rate, weight, age } = props
-      return Math.round(
-        ((MALE_MODIFIER + MALE_RATE_COEF * rate - MALE_WEIGHT_COEF * weight + MALE_AGE_COEF * age) /
-          REDUCER) *
-          HOUR,
-      )
-    },
-  },
-
-  [FEMALE]: {
-    using(props) {
-      const { rate, weight, age } = props
-      return Math.round(
-        ((FEMALE_MODIFIER +
-          FEMALE_RATE_COEF * rate -
-          FEMALE_WEIGHT_COEF * weight +
-          FEMALE_AGE_COEF * age) /
-          REDUCER) *
-          HOUR,
-      )
-    },
-  },
+  [MALE]: calculator(MALE_MODIFIER, MALE_RATE_COEF, MALE_WEIGHT_COEF, MALE_AGE_COEF),
+  [FEMALE]: calculator(FEMALE_MODIFIER, FEMALE_RATE_COEF, FEMALE_WEIGHT_COEF, FEMALE_AGE_COEF),
   [UNKNOWN]: {
     using(props) {
       const male = methods[MALE].using(props)
