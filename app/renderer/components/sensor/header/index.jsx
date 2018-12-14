d288f931ee75db789ead2c4cac25ba74f7fe52b9
copios <copios.jp@gmail.com>
2018-12-15 00:38:01 +0900

    fix(release): bump version

--- app/renderer/components/sensor/header/index.jsx
+++ app/renderer/components/sensor/header/index.jsx
@@ -4,6 +4,7 @@ import ActivityIndicator from '../activity_indicator'
 import { withStyles } from '@material-ui/core/styles'
 import styles from '../../../styles/'
 import StopWatch from '../../stop_watch/'
+import RecordButton from '../record_button/'
 
 export const Header = (props) => {
   const { classes, sensor } = props
@@ -11,6 +12,7 @@ export const Header = (props) => {
     <div className={classes.cardHeader}>
       <ActivityIndicator fontSize="small" active={sensor.active} />
       <div className={classes.cardName}>{sensor.name}&nbsp;</div>
+      <RecordButton sensor={sensor} />
       <StopWatch sensor={sensor} />
     </div>
   )
